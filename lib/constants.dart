91aa89786a6ad7d82db34275f2c148587cbd0a2e
awesomeroks <30774147+awesomeroks@users.noreply.github.com>
2020-12-07 02:14:57 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -10,6 +10,6 @@ var pingUrl = 'http://104.211.73.17:8080/translator/translate';
 List<String> descriptions = [
   "Given integers var0 , var1 . if var0 does not exceed var1 then return var0 . let var2 be the smallest value such that ans * ( ans + 1 ) / 2 is not less than n - m . return var2 plus var1 . ",
   "[ map a [ partial1 b - ] ]",
-  "Consider an array of numbers a , what is the length of the longest subsequence of a that starts with 1 and increases by 1",
+  "Given array var1 . if its smallest element equals 1 , return - 1 . otherwise , return 1 . ",
   "Input code here...",
 ];
