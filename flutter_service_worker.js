7142d2566d21e6da81441b6fb3c74a7995e5b12d
awesomeroks <30774147+awesomeroks@users.noreply.github.com>
2020-12-06 23:53:57 +0530

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -16,8 +16,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "9c3b8405cd32341b4250a960f040d8e3",
-"/": "9c3b8405cd32341b4250a960f040d8e3",
+"index.html": "d3b2752d401dfbeca7bb2777474d3155",
+"/": "d3b2752d401dfbeca7bb2777474d3155",
 "main.dart.js": "f881baba0565f17d53eb9dfc4b37ef30",
 "manifest.json": "dedebe9e5e14c02a0e24bc20c5b7a2bd",
 "version.json": "cd9aff8e57d8a329a9bc18e8b0909526"
