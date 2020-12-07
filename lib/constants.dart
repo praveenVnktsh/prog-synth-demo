2e1698cd2fa5fcec5fe7d758b5c8625eac8dedad
awesomeroks <30774147+awesomeroks@users.noreply.github.com>
2020-12-07 10:12:33 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 var primaryColor = Color(0xFF93c845);
 var secondaryColor = Color(0xFF052744);
 
-var pingUrl = 'https://104.211.73.17:8080/translator/models';
+var pingUrl = 'http://104.211.73.17:8080/translator/translate';
 
 List<String> descriptions = [
   "Given integers var0 , var1 . if var0 does not exceed var1 then return var0 . let var2 be the smallest value such that ans * ( ans + 1 ) / 2 is not less than n - m . return var2 plus var1 . ",
